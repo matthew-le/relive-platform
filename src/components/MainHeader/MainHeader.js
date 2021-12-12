093af9473de6134e1521450b14ae1e6ffb99f60e
Matthew Le <matthewthaile@gmail.com>
2021-12-11 23:15:35 -0800

    first init

---
+++ src/components/MainHeader/MainHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './MainHeader.module.css';
+
+const MainHeader = (props) => {
+return (
+    <header className={classes['main-header']}>
+        <h1>Relive</h1>
+    </header>
+    );
+};
+
+export default MainHeader;
