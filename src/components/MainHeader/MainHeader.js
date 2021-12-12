efdd3a7f8551bed18d994091e38752b2ace56908
Matthew Le <matthewthaile@gmail.com>
2021-12-12 01:06:27 -0800

    adding components

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,12 +1,15 @@
 import React from 'react';
+
+import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
 
 const MainHeader = (props) => {
-return (
+  return (
     <header className={classes['main-header']}>
-        <h1>Relive</h1>
+      <h1>Relive</h1>
+      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
     </header>
-    );
+  );
 };
 
 export default MainHeader;
