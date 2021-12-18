d0313390768ee90113d77ccf9676a0b56ad10401
Matthew Le <matthewthaile@gmail.com>
2021-12-18 14:10:57 -0800

    creating and using useContext

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -25,10 +25,6 @@ const passwordReducer = (state, action) => {
 };
 
 const Login = (props) => {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [emailIsValid, setEmailIsValid] = useState();
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -36,7 +32,7 @@ const Login = (props) => {
     isValid: null,
   });
 
-  const[passwordState, dispatchPassword] =useReducer(passwordReducer, {
+  const[passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: '',
     isValid: null,
   });
@@ -67,17 +63,10 @@ const Login = (props) => {
 
   const emailChangeHandler = (event) => {
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
-
-    // setFormIsValid(
-    //   event.target.value.includes('@') && passwordState.isValid
-    //   );
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({type: 'USER_INPUT', val:event.target.value});
-
-    // setFormIsValid(
-    //   emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
